ebfc6ce2b04e261966ccdae7f6e5bf75f9b8facc
oluwaseun501 <ogunmokunoluwaseun501@gmail.com>
2026-06-29 16:53:22 +0100

    fix: switch to SWC plugin"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'   // ← change this line
+import react from '@vitejs/plugin-react-swc'   
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+
 
 
 export default defineConfig({
