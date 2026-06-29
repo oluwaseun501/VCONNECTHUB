dff1676fb0a437228f7d76ba6d8ed81b419cd0a1
oluwaseun501 <ogunmokunoluwaseun501@gmail.com>
2026-06-29 16:39:40 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'   // ← change this line
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
